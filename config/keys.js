14a604f1d2d95e1f3d03539479b6dfb33e565532
unknown <amannsiddhartha@gamil.com>
2019-06-22 10:45:03 +0530

    handled auth

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:'mongodb+srv://aman:#321namA@cluster0-kot7e.mongodb.net/test?retryWrites=true&w=majority',
+  googleClientID:'818724064368-ttrjamj10pb1s6pt3ejd0vl7jv3p7mf9.apps.googleusercontent.com',
+  googleClientSecret:'hUrMoxt0R42V6EnJ_l7lprXp'
+}
