96bcdaff48d8928b3f206aae1cd1d0ad55770977
unknown <amannsiddhartha@gamil.com>
2019-06-22 11:28:55 +0530

    google auth correction

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mongoURI:'mongodb+srv://aman:Aman123#@cluster0-kot7e.mongodb.net/test?retryWrites=true&w=majority',
+  mongoURI:'mongodb+srv://aman_story:amanstory@cluster0-kot7e.mongodb.net/test?retryWrites=true&w=majority',
   googleClientID:'818724064368-ttrjamj10pb1s6pt3ejd0vl7jv3p7mf9.apps.googleusercontent.com',
   googleClientSecret:'hUrMoxt0R42V6EnJ_l7lprXp'
 }
