91b1ac5d8ed5b8676cc3ff9fa610db0365f249d8
unknown <amannsiddhartha@gamil.com>
2019-06-24 10:04:50 +0530

    handle auth resolving

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,7 @@ const passport = require('passport');
 
 router.get('/google', passport.authenticate('google', {scope: ['profile', 'email']}));
 
-router.get('/google/callback', 
+router.get('/google/callback',
   passport.authenticate('google', { failureRedirect: '/' }),(req, res) => {
     res.redirect('/dashboard');
   });
