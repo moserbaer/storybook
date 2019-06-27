c84867393d7a962ce0ca9dc9f3867195f9d3122f
unknown <amannsiddhartha@gamil.com>
2019-06-27 12:01:10 +0530

    functionality

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const path = require('path');
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const passport = require('passport');
-
-//load user model
+//load model
 require('./models/User');
+require('./models/Story');
 
 // Passport Config
 require('./config/passport')(passport);
@@ -14,9 +17,18 @@ require('./config/passport')(passport);
 // Load Routes
 const index = require('./routes/index');
 const auth = require('./routes/auth');
+const stories = require('./routes/stories');
 
 // Load Keys
 const keys = require('./config/keys');
+
+// Handlebars Helpers
+const {
+  truncate,
+  stripTags,
+  formatDate,
+  select
+} = require('./helpers/hbs');
 
 // Map global promises
 mongoose.Promise = global.Promise;
@@ -28,13 +40,24 @@ mongoose.connect(keys.mongoURI,{ 
 
 const app = express();
 
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// MEthod Override Middelware
+app.use(methodOverride('_method'));
+
 // Handlebars Middleware
 app.engine('handlebars', exphbs({
+  helpers: {
+    truncate: truncate,
+    stripTags: stripTags,
+    formatDate:formatDate,
+    select:select
+  },
   defaultLayout:'main'
 }));
 app.set('view engine', 'handlebars');
-
-
 
 app.use(cookieParser());
 app.use(session({
@@ -43,7 +66,7 @@ app.use(session({
   saveUninitialized: false
 }));
 
-//passport MIddleware
+// Passport Middleware
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -53,11 +76,13 @@ app.use((req, res, next) => {
   next();
 });
 
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Use Routes
 app.use('/', index);
 app.use('/auth', auth);
-
+app.use('/stories', stories);
 
 const port = process.env.PORT || 5000;
 
--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  mongoURI:'mongodb+srv://aman_story:amanstory@cluster0-kot7e.mongodb.net/test?retryWrites=true&w=majority',
+  mongoURI:'mongodb+srv://aman_story:amanstory@cluster0-kot7e.mongodb.net/storybooks?retryWrites=true&w=majority',
   googleClientID:'398947424939-g06r17lp5cb4j2rf8i9leblmnomek2lf.apps.googleusercontent.com',
   googleClientSecret:'cMiD9EDNNfpY8zECYkIVshJg'
 }
